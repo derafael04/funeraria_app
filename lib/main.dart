b237aa9692b508ee2746df857f957a3e328211c9
rafaeloliveira <derafael04@gmail.com>
2019-05-13 13:15:38 -0300

    Alterei a cor de fundo para azul

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ void main(){
 
   runApp(MaterialApp(
       title: 'Temos que escolher um nome para o APP rsrs',
-      home: Container(color: Colors.white,)
+      home: Container(color: Colors.blue,)
   )); //MaterialApp
 }
